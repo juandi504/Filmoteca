8d7bad75b989962eafc07430974676f3e22b0373
JuanDiego <jdiego.faci@gmail.com>
2025-01-02 16:00:50 +0100

    Commit Ejercicio 6. Añadiendo contenido a la pantalla

--- app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.campusdigitalfp.filmoteca
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.ModifierLocal
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +35,68 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FilmotecaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AboutScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
+
+@Composable
+fun AboutScreen() {
+    val context = LocalContext.current // Obtener el contexto local
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center // Centra verticalmente
+    ) {
+        Text(
+            text = "Creada por Juan Diego Faci Muñoz",
+            modifier = Modifier.padding(16.dp),
+
+            )
+        Image(
+            painter = painterResource(id = R.drawable.perfil),
+            contentDescription = "Imagen de perfil",
+            modifier = Modifier.size(100.dp),
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(
+                onClick = { showToast(context, "Funcionalidad sin implementar") },
+                modifier = Modifier.padding(vertical = 8.dp)
+            ) {
+                Text("Ir al sitio web")
+            }
+            Button(
+                onClick = { showToast(context, "Funcionalidad sin implementar") },
+                modifier = Modifier.padding(vertical = 8.dp)
+            ) {
+                Text("Obtener soporte")
+            }
+        }
+        Button(
+            onClick = { showToast(context, "Funcionalidad sin implementar") },
+            modifier = Modifier.padding(vertical = 8.dp)
+        ) {
+            Text("Volver")
+        }
+
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FilmotecaTheme {
-        Greeting("Android")
+        AboutScreen()
     }
 }
