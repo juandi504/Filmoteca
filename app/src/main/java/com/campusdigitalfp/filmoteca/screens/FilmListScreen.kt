9274ee0f22d59f38bea92036792de7e908bb3bdb
JuanDiego <jdiego.faci@gmail.com>
2025-01-07 12:41:25 +0100

    Commit Ejercicio 16. Listado de películas con diseño propio

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmListScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmListScreen.kt
@@ -1,10 +1,17 @@
 package com.campusdigitalfp.filmoteca.screens
 
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -12,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -38,21 +47,64 @@ fun FilmListScreen(navController: NavHostController) {
             )
         }
     ) { innerPadding ->
-        // LazyColumn
-        LazyColumn(
+        // Usamos una columna para organizar la lista y el botón acerca de
+        Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(16.dp),
-            //verticalArrangement = Arrangement.spacedBy(8.dp)
+                .padding(8.dp)
         ) {
-            // Iteramos cada elemento que tiene la lista mutable de FilmDataSource
-            // y mediante la función lambda usamos para cada elemento la función toString()
-            items(FilmDataSource.films) { film ->
-                Text(
-                    text = film.toString(), // Usando el método toString() de Film
-                    modifier = Modifier.padding(vertical = 8.dp)
-                )
+            // LazyColumn para ir mostrando los elementos de la pantalla en ese momento
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(8.dp),
+            ) {
+                // Iteramos cada elemento que tiene la lista mutable de FilmDataSource
+                // y mediante la función lambda mostramos la imagen de portada y el título
+                // y director (titulo-director) organizado en una columna
+                items(FilmDataSource.films) { film ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        // Imagen del cartel de la película
+                        Image(
+                            painter = painterResource(film.imageResId),
+                            contentDescription = film.title,
+                            modifier = Modifier
+                                .size(width = 70.dp, height = 140.dp)
+                                .padding(end = 4.dp)
+                                .fillMaxHeight()
+                        )
+                        Column(
+                            modifier = Modifier
+                                .weight(1f) // Reparte espacio con la imagen
+                                .padding(start = 10.dp),
+                            horizontalAlignment = Alignment.Start
+                        ) {
+                            Text(
+                                text = film.toString(), // Usando el metodo toString() de film
+                                style = MaterialTheme.typography.headlineMedium,
+                                modifier = Modifier.padding(vertical = 4.dp),
+
+                                )
+                            Text(
+                                text = film.director ?: "Desconocido",
+                                style = MaterialTheme.typography.bodyMedium,
+                            )
+                        }
+                    }
+                }
+            }
+            // Fuera del LazyColumn se sitúa el botón acerca de
+            Button(
+                onClick = { navController.navigate("about") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp)
+            ) {
+                Text(stringResource(id = R.string.Acerca_de))
             }
         }
     }
